31fa70a05631f9af724534eb96514d080a8ee9c6
twjin03 <twjindev@gmail.com>
2025-05-08 01:25:26 +0900

    #6.4 Cleanup

--- NomadCoders/react-begin/src/App.js
+++ NomadCoders/react-begin/src/App.js
@@ -1,37 +1,36 @@
 import { useState, useEffect } from 'react';
-import Button from "./Button";
-import styles from './App.module.css';
+
+
+function Hello() {
+  // useEffect(() => {
+  //   console.log('created !');
+  //   return () => console.log('destroyed ...');
+  // }, []);
+
+  // function byFn() {
+  //   console.log('bye ...');
+  // };
+  // function hiFn() {
+  //   console.log('hi !');
+  //   return byFn;
+  // };
+  useEffect(() => {
+    console.log('hi !');
+    return () => console.log('bye ...');
+  }, []);
+  return <h1>Hello</h1>
+}
 
 function App() {
-  const [counter, setValue] = useState(0); // useState()는 array를 반환 
-  const [keyword, setKeyword] = useState(""); // value, function
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  // console.log('i run all the time');
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {
+    setShowing((prev) => !prev);
+  };
 
-  useEffect(() => { // 한 번만 실행되도록 코드를 보호함 
-    console.log('i run only once');
-  }, []);
-  useEffect(() => { 
-    // if (keyword !== "" && keyword.length > 5) {
-    console.log('keyword 변경 시 실행');
-    // }
-  }, [keyword]); // keyword를 지켜보고 있음 
-  useEffect(() => {
-    console.log('counter 변경 시 실행');
-  }, [counter]);
-  useEffect(() => {
-    console.log('keyword or counter 변경 시 실행');
-  }, [keyword, counter]);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type='text'
-        placeholder='Search here... ' />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>  {showing ? 'Hide' : 'Show'} </button>
     </div>
   );
 }
