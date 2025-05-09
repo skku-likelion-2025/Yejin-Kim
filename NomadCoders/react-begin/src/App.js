23b2cc48bc935fda5502c5ffe9e14e1ae63825ac
twjin03 <twjindev@gmail.com>
2025-05-09 16:18:21 +0900

    #7.0 To Do List part One

--- NomadCoders/react-begin/src/App.js
+++ NomadCoders/react-begin/src/App.js
@@ -1,36 +1,29 @@
 import { useState, useEffect } from 'react';
-
-
-function Hello() {  
-  // useEffect(() => {
-  //   console.log('created !');
-  //   return () => console.log('destroyed ...');
-  // }, []);
-
-  // function byFn() {
-  //   console.log('bye ...');
-  // };
-  // function hiFn() {
-  //   console.log('hi !');
-  //   return byFn;
-  // };
-  useEffect(() => {
-    console.log('hi !');
-    return () => console.log('bye ...');
-  }, []);
-  return <h1>Hello</h1>
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => {
-    setShowing((prev) => !prev);
-  };
-
+  const [toDo, setToDo] = useState('')
+  const [toDos, setToDos] = useState([]);
+  const onChange = (e) => setToDo(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log({ toDo });
+    if (toDo === '') {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo('');
+    console.log({...toDos});
+  }
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>  {showing ? 'Hide' : 'Show'} </button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type='text'
+          placeholder='write your todo...' />
+      </form>
+      <button>Add Todo</button>
     </div>
   );
 }
