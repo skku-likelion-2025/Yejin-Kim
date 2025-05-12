11a0e16a3a2b5faa9c9bf8afd460a807c7181b36
twjin03 <twjindev@gmail.com>
2025-05-12 22:59:35 +0900

    #7.1 To Do List part Two

--- NomadCoders/react-begin/src/App.js
+++ NomadCoders/react-begin/src/App.js
@@ -9,10 +9,10 @@ function App() {
     if (toDo === '') {
       return;
     }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo('');
+    setToDos((currentArray) => [toDo, ...currentArray]); // 힘수를 보냄, 현재 state를 새로운 state 계산에 사용 
+    setToDo(''); // value를 보냄 
   }
-  console.log({ ...toDos });
+  console.log(toDos);
   return (
     <div>
       <h1>My To Dos ({toDos.length})</h1>
@@ -24,7 +24,9 @@ function App() {
           placeholder='write your todo...' />
         <button>Add Todo</button>
       </form>
-
+      <hr />
+      {toDos.map((item, index) => <li key={index}>{item}</li>)} 
+      {/* array의 element들을 바꿀 때 map은 함수를 넣을 수 있도록 함 map은 모든 element들에 대해 실행됨  */}
     </div>
   );
 }
